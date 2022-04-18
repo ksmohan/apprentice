38ee2fd46f1347d74c8fddc53ea6356376a0791c
Holger Schulz <holger.schulz@durham.ac.uk>
2022-04-18 18:30:06 +0100

    tons of improvements

--- apprentice/function.py
+++ apprentice/function.py
@@ -18,25 +18,59 @@ class Function(object):
                 self.fnspace_ = fnspace
             else:
                 try:
-                    self.fnspace_ = space.Space(space)
+                    self.fnspace_ = space.Space(fnspace)
                 except Exception as e:
                     print("Unable to interpret space argument:", e)
+
+        # The currentpoint where we evaluate the function at.
+        self.currpoint_ = np.empty(self.dim_, dtype=np.float64)
+
+        # Set up bounds for the parameter space
         self.bounds_ = np.zeros((dim,2))
-        for d in range(dim):
-            self.bounds[d][0] = self.fnspace_.a_[d]
-            self.bounds[d][1] = self.fnspace_.b_[d]
+        if 'bounds' in kwargs:    self.set_bounds(kwargs['bounds'][0], kwargs['bounds'][1])
+        else:                     self.set_bounds(self.fnspace_.a_   , self.fnspace_.b_)
+
+        # Fix parameters
+        self.fixed_indices_ = ([],)
+        self.fixed_values_  = []
+        if 'fixed' in kwargs: self.set_fixed_parameters(kwargs["fixed"])
+        else:                 self.set_fixed_parameters([])
+
+
+    def set_bounds(self, bmin, bmax):
+        """
+        Expect min, max
+        """
+        for d in range(self.dim_):
+            self.bounds_[d][0] = bmin[d]
+            self.bounds_[d][1] = bmax[d]
+
+    def set_fixed_parameters(self, fixed):
+        """
+        list of tuples, (index, value)
+        """
+        self.fixed_indices_ = ([fx[0] for fx in fixed],)
+        self.fixed_values_  =  [fx[1] for fx in fixed]
+
+        for fx in fixed: self.currpoint_[fx[0]] = fx[1]
+
+        self.free_indices_  = ([i for i in range(self.dim_) if not i in self.fixed_indices_[0]],)
+
+        # TODO add debug message!
+
+
 
     @classmethod
     def mk_empty(cls, dim):
         return cls(dim)
 
     @classmethod
-    def from_space(cls, spc):
+    def from_space(cls, spc, **kwargs):
         if Util.inherits_from(spc, 'Space'):
-            return cls(spc.dim, spc)
+            return cls(spc.dim, spc, **kwargs)
         else:
             try:
-                return cls(len(spc), space.Space.fromList(spc))
+                return cls(len(spc), space.Space.fromList(spc), **kwargs)
             except Exception as e:
                 print("Unable to interpret list argument as Space")
 
@@ -86,5 +120,10 @@ class Function(object):
         raise Exception("The function objective must be implemented in the derived class")
 
     def __call__(self, x):
-        return self.objective(x)
+        """
+        If we have N fixed parameters, and the dim is D, the x is expected to be
+        of size D - N
+        """
+        self.currpoint_[self.free_indices_] = x
+        return self.objective(self.currpoint_)
 
